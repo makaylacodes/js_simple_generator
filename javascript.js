198ecfeff0f23055a6bae906fced337e84c23082
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-07-28 14:46:12 +0900

    I have made all the final edits for this project. May add more quotes later

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 
 var advice = [
-    'You should check todays newspaper for a job ',
-    'Always leave a spare key under your doormat',
-    'Dont wait for your coffee to cool down! Chug it :)',
-    'Get a turtle for companionship',
-    'Treat yourself to a shopping spree, even when youre broke',
-    'You can turn left at a red light if no one is around',
-    'It is okay to swim with sharks if you have open wounds',
-    'You do not have to look both ways before crossing the street'
+    'You should check today\'s newspaper for a job. ',
+    'Always leave a spare key under your doormat.',
+    'Don\'t wait for your coffee to cool down! Chug it :)',
+    'Get a turtle for companionship.',
+    'Treat yourself to a shopping spree, even when you\'re broke.',
+    'You can turn left at a red light if no one is around.',
+    'It is okay to swim with sharks if you have open wounds.',
+    'You do not have to look both ways before crossing the street.',
+    'Not sure if the milk is spoiled? Take a sip :D',
+    'Keep cake moist by eating it all in one sitting.',
+    'If you run out of eyeliner, use a sharpie.',
+    'When cutting onions, remember to occaisionally touch your eyes.'
 ]
 
 
